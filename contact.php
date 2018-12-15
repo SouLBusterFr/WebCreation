8ae231c7e114b5682a406b26105d5c83ef72419b
Alexandre Sillani <soulbuster68@gmail.com>
2018-12-15 16:15:10 +0100

    First commit, Page de contact OK + css OK + Lancement du html

---
+++ contact.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="test">
+        <div id="titre"><span >Contact Us</span></div>
+<div>
+    <form action="index.php" method="post">
+  <div class="input">
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" name="nom" />
+  </div>
+  <div class="input">
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" />
+  </div>
+  <div class="input">
+    <label for="formation">Formation :</label>
+    <input type="text" id="formation" name="formation" />
+  </div>
+  <div class="input">
+      <label for="subject">Sujet :</label>
+      <input type ="text" id="subject" name="subject" />
+  </div>
+  <div class="input">
+    <label for="mail">E-mail :</label>
+    <input type="email" id="mail" name="mail" />
+  </div>
+  <div class="input">
+    <label for="message">Message:</label>
+    <textarea id="message" name="message"></textarea>
+  </div>
+  <div class="button">
+        <input type="submit" name="valider" value="envoyer" id="button"/>
+  </div>
+ </div>
+    <?php
+
+    ini_set( 'display_errors', 1 );
+
+    error_reporting( E_ALL );
+
+    if(isset($_POST['valider'])){
+            $from = "test@mesozogeek.000webhostapp.com";
+            $to = "maurin.margail@ynov.com";
+            $mail = $_POST['mail'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $formation = $_POST['formation'];
+            $headers = "From " . $nom . " " . $prenom . " en " . $formation . "  " . $mail;
+            mail($to,$subject,$message, $headers);
+            echo "L'email a été envoyé.";
+        }
+?>
+</body>
